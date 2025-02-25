4b1afeaa40579e77aa3541025514196d3bbf9026
Rajvaibhav Rahane <rrahane@amazon.com>
2025-02-25 12:18:21 -0800

    add test placeholder
    Signed-off-by: Rajvaibhav Rahane <rrahane@amazon.com>

---
+++ test_remote_vector_index_builder/test_placeholder.py
@@ -0,0 +1,10 @@
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+def test_placeholder():
+    print("Hello World")
+    assert 1 + 1 == 2
