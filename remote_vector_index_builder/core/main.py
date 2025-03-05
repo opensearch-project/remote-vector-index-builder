387fa821d5db374adae9de403017d00cedc10fc3
Rohan Chitale <rchital@amazon.com>
2025-03-05 11:08:40 -0800

    Modified dockerfile to have pythonpath. Added example main.py
    Signed-off-by: Rohan Chitale <rchital@amazon.com>

--- remote_vector_index_builder/core/main.py
+++ remote_vector_index_builder/core/main.py
@@ -5,13 +5,47 @@
 # this file be licensed under the Apache-2.0 license or a
 # compatible open source license.
 
-# TODO: Call each task from tasks.py in sequence, main()
-# Add this file as the entry point for the Dockerfile
+from core.common.models.index_build_parameters import IndexBuildParameters
+import time
+from core import create_vectors_dataset, upload_index
+import logging
+from io import BytesIO
+
+
+def configure_logging(log_level):
+    logging.basicConfig(
+        level=log_level,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
 
 def main():
-    pass
+    index_build_params = {
+        "repository_type": "s3",
+        "container_name": "testbucket-rchital",
+        "vector_path": "target_field__53.knnvec",
+        "doc_id_path": "target_field__53.knndid",
+        "dimension": 1000,
+        "doc_count": 76800,
+    }
+
+    vector_bytes_buffer = BytesIO()
+    doc_id_bytes_buffer = BytesIO()
+    model = IndexBuildParameters.model_validate(index_build_params)
+    start_time = time.time()
+    create_vectors_dataset(
+        model, {"debug": True}, vector_bytes_buffer, doc_id_bytes_buffer
+    )
+    end_time = time.time()
+    print(end_time - start_time)
+
+    # start_time = time.time()
+    # upload_index(model, {'debug': True}, "./target_field__53.knnvec")
+    # end_time = time.time()
+    # print(end_time - start_time)
 
 
 if __name__ == "__main__":
+    configure_logging("INFO")
     main()
